5e4f88ce3466d006058f6f52f985e349527687a7
tbatchelli <tbatchelli@acm.org>
2024-12-01 15:16:58 -0800

    `feat: Add footnotes support in markdown conversion`

--- src/quickblog/internal.clj
+++ src/quickblog/internal.clj
@@ -136,6 +136,7 @@
     (-> markdown
         pre-process-markdown
         (md/md-to-html-string-with-meta :reference-links? true
+                                        :footnotes? true
                                         :code-style
                                         (fn [lang]
                                           (format "class=\"lang-%s\"" lang)))
